0091bd95b1556eb2621f1a6240c1b85616147217
feargrinn <tobyrogers000@gmail.com>
2024-12-06 18:07:21 +0100

    player moves albeit in a shitty way kinda
    i mean to fix this, kinda messy code for now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,70 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputReader input;
     [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
+    [SerializeField] private float maxJumpHeight;
+    [SerializeField] private LayerMask m_WhatIsGround;
+    [SerializeField] private Transform m_GroundCheck;
 
-    private Vector2 _moveDirection;
-    private bool _isJumping;
+    private Rigidbody2D m_Rigidbody2D;
+    private Vector2 _moveDirection; // leaving is as vec2 instead of axis bc i assume the y axis can be useful in other interactions
+    private bool _isJumping; // REMEMBER the longer you press space the higher the jump (up until an upper limit)
+    private float _currentJumpHeight = 0.0f;
+    
+    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	private bool m_Grounded;            // Whether or not the player is grounded.
 
+    public UnityEvent OnLandEvent;
     private void Start()
     {
         input.MoveEvent += HandleMove;
         input.JumpEvent += HandleJump;
         input.JumpCancelledEvent += HandleJumpCancelled;
+
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+
+		if (OnLandEvent == null)
+        {
+			OnLandEvent = new UnityEvent();
+        }
     }
     private void Update()
     {
         Move();
         Jump();
+
+        if (_isJumping)
+        {
+            _currentJumpHeight += jumpSpeed * Time.deltaTime;
+        }
+
+        if (_currentJumpHeight >= maxJumpHeight || _isJumping == false)
+        {
+            _isJumping = false;
+            _currentJumpHeight = 0.0f;
+        }
+        
+        bool wasGrounded = m_Grounded;
+		m_Grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCapsuleAll(m_GroundCheck.position, new Vector2(0, k_GroundedRadius), CapsuleDirection2D.Vertical, m_WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				m_Grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
+		}
     }
     private void HandleMove(Vector2 dir)
     {
@@ -39,13 +84,13 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-        transform.position += new Vector2(_moveDirection.x, 0) * (speed * Time.deltaTime);
+        transform.position += new Vector3(_moveDirection.x, 0, 0) * (speed * Time.deltaTime);
     }
     private void Jump()
     {
         if (_isJumping)
         {
-            transform.position += new Vector2(0, 1) * (jumpSpeed * Time.deltaTime);
+            transform.position += new Vector3(0, 1, 0) * (jumpSpeed * Time.deltaTime);
         }
     }
 }
